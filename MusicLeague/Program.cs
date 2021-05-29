c0be6b23a2d5eae7498ed57dbe01cc0a4c49b161
Matheus Andrade Fernal <matheusfernal@gmail.com>
2021-05-29 13:24:53 -0300

    Showing votes by user

--- MusicLeague/Program.cs
+++ MusicLeague/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using MusicLeague.Repository;
 
 namespace MusicLeague
@@ -9,7 +10,21 @@ namespace MusicLeague
         {
             var repository = new DataRepository("Data/competitors.csv", "Data/rounds.csv", "Data/submissions.csv", "Data/votes.csv");
 
-            Console.WriteLine("Hello World!");
+            // Total votes by competitor
+            foreach(var competitor in repository.Competitors)
+            {
+                Console.WriteLine($"{competitor.Name} votes from:");
+                foreach(var voter in repository.Competitors)
+                {
+                    var competitorSubmissions = repository.Submissions.Where(s => s.SubmitterId == competitor.Id).Select(s => s.SpotifyUri);
+                    var competitorPoints = repository.Votes.Where(v => competitorSubmissions.Contains(v.SpotifyUri) && v.VoterId == voter.Id).Sum(v => v.PointsAssigned);
+
+                    Console.WriteLine($"  {voter.Name}: {competitorPoints}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("End");
         }
     }
 }
