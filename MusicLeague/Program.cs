9147469dface39b2c0158f2fc83f44ab0c026271
Matheus Andrade Fernal <matheusfernal@gmail.com>
2021-05-31 09:11:01 -0300

    Correction on repeated music

--- MusicLeague/Program.cs
+++ MusicLeague/Program.cs
@@ -11,13 +11,13 @@ namespace MusicLeague
             var repository = new DataRepository("Data/competitors.csv", "Data/rounds.csv", "Data/submissions.csv", "Data/votes.csv");
 
             // Total votes by competitor
-            foreach(var competitor in repository.Competitors)
+            foreach (var competitor in repository.Competitors)
             {
                 Console.WriteLine($"{competitor.Name}'s votes from:");
-                foreach(var voter in repository.Competitors)
+                foreach (var voter in repository.Competitors)
                 {
-                    var competitorSubmissions = repository.Submissions.Where(s => s.SubmitterId == competitor.Id).Select(s => s.SpotifyUri);
-                    var competitorPoints = repository.Votes.Where(v => competitorSubmissions.Contains(v.SpotifyUri) && v.VoterId == voter.Id).Sum(v => v.PointsAssigned);
+                    var competitorSubmissions = repository.Submissions.Where(s => s.SubmitterId == competitor.Id).Select(s => s.RoundId + s.SpotifyUri);
+                    var competitorPoints = repository.Votes.Where(v => competitorSubmissions.Contains(v.RoundId + v.SpotifyUri) && v.VoterId == voter.Id).Sum(v => v.PointsAssigned);
 
                     Console.WriteLine($"  {voter.Name}: {competitorPoints}");
                 }
